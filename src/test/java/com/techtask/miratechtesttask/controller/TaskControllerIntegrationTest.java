4625de9d84ff73d0bd15e34c7fada05f20970757
Danyl Gryn <gryndanya@gmail.com>
2024-04-26 15:44:16 +0300

    commit

--- src/test/java/com/techtask/miratechtesttask/controller/TaskControllerIntegrationTest.java
+++ src/test/java/com/techtask/miratechtesttask/controller/TaskControllerIntegrationTest.java
@@ -1,37 +1,43 @@
 package com.techtask.miratechtesttask.controller;
 
 import com.techtask.miratechtesttask.repository.TaskRepository;
-import com.techtask.miratechtesttask.service.TaskService;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@AutoConfigureTestDatabase
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Testcontainers
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 public class TaskControllerIntegrationTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @MockBean
+    private TaskRepository taskRepository;
 
     @Autowired
-    private TaskRepository taskRepository;
-    @MockBean
-    private TaskService taskService;
+    TaskController taskController;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @BeforeEach
     void clearDatabase(@Autowired Flyway flyway) {
         flyway.clean();
         flyway.migrate();
     }
+
+
+
 }
